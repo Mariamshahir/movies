46973d9aa75ef109727f9458c5420922bf114458
Mariam Shahir <mariamshahir2000@gmail.com>
2024-04-03 13:46:59 +0200

    Finished utils app.

---
+++ lib/utils/colors_app.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class AppColors{
+  static const Color white = Colors.white;
+  static const Color navigaterBar = Color(0xff1A1A1A);
+  static const Color search = Color(0xff514F4F);
+  static const Color selectIcon = Color(0xffFFBB3B);
+  static const Color unSelectIcon = Color(0xffC6C6C6);
+
+}
---
+++ lib/utils/theme_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:test_movies/utils/colors_app.dart';
+
+abstract class AppTheme{
+  static const TextStyle title = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+  );
+}
