a594844b6ec7d5d2dfd8bff9177e9c1042bc9f6b
Mariam Shahir <mariamshahir2000@gmail.com>
2024-04-03 13:46:01 +0200

    Finished error and loading widget.

---
+++ lib/widgets/app_error.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppError extends StatelessWidget {
+  final String error;
+  final Function? onRefreshClick;
+
+  const AppError({
+    super.key,
+    required this.error,
+    this.onRefreshClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(error,style: TextStyle(color: Colors.white),),
+          ElevatedButton(onPressed: () {
+            onRefreshClick?.call();
+          }, child: const Text("Refresh"))
+        ],
+      ),
+    );
+  }
+}
