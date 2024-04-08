ccd74334246bc89d0ac1305a96edd802f5c36942
Mariam Shahir <mariamshahir2000@gmail.com>
2024-04-08 19:48:15 +0200

    Finished utils app.

--- lib/utils/colors_app.dart
+++ lib/utils/colors_app.dart
@@ -4,9 +4,12 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors{
   static const Color white = Colors.white;
-  static const Color navigaterBar = Color(0xff1A1A1A);
+  static const Color part = Color(0xff1A1A1A);
   static const Color search = Color(0xff514F4F);
   static const Color selectIcon = Color(0xffFFBB3B);
   static const Color unSelectIcon = Color(0xffC6C6C6);
-
+  static const Color gray = Color(0xffB5B4B4);
+  static const Color navigaterBar = Color(0xff2a2a2a);
+  static const Color bookMark =Color(0xb3514f4f);
+  static const Color divider =Color(0xff707070);
 }
--- lib/utils/theme_app.dart
+++ lib/utils/theme_app.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:test_movies/utils/colors_app.dart';
 
-abstract class AppTheme{
+abstract class AppTheme {
   static const TextStyle title = TextStyle(
     fontSize: 15,
     fontWeight: FontWeight.w400,
     color: AppColors.white,
   );
+  static const TextStyle tagline = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+  );
+  static const TextStyle movieTitle = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+  );
+  static const TextStyle titleName = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+  );
+  static const TextStyle watchList = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+  );
+  static const TextStyle time = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: AppColors.gray,
+  );
+
+  static ThemeData lightTheme = ThemeData(
+      primaryColor: Colors.black,
+      appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)))),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: AppColors.white),
+        titleMedium: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.normal,
+            color: AppColors.white),
+        titleSmall: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.w400,
+            color: AppColors.gray),
+      ));
 }
