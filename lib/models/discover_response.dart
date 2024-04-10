295f62f0fe8619dee382ec4532b4a32179e21345
Mariam Shahir <mariamshahir2000@gmail.com>
2024-04-10 15:55:19 +0200

    Finished browse widget.

--- lib/models/discover_response.dart
+++ lib/models/discover_response.dart
@@ -46,18 +46,3 @@ class DiscoverResponse {
     return map;
   }
 }
-
-/// adult : false
-/// backdrop_path : "/j3Z3XktmWB1VhsS8iXNcrR86PXi.jpg"
-/// genre_ids : [28,878,12,14]
-/// id : 823464
-/// original_language : "en"
-/// original_title : "Godzilla x Kong: The New Empire"
-/// overview : "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own."
-/// popularity : 3269.222
-/// poster_path : "/tMefBSflR6PGQLv7WvFPpKLZkyk.jpg"
-/// release_date : "2024-03-27"
-/// title : "Godzilla x Kong: The New Empire"
-/// video : false
-/// vote_average : 6.734
-/// vote_count : 521
