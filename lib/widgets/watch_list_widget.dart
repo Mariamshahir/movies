13f41378b638b6c77c546249a26fb3674506e339
Mariam Shahir <mariamshahir2000@gmail.com>
2024-04-12 01:41:39 +0200

    Finished watch list widget.

--- lib/widgets/watch_list_widget.dart
+++ lib/widgets/watch_list_widget.dart
@@ -1,20 +1,28 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:test_movies/firebase/firebase.dart';
 import 'package:test_movies/models/film.dart';
 import 'package:test_movies/utils/colors_app.dart';
 import 'package:test_movies/utils/theme_app.dart';
 import 'package:test_movies/widgets/movie_details_widget/movie_view_widget.dart';
 
-class WatchListWidget extends StatelessWidget {
+class WatchListWidget extends StatefulWidget {
   final Film result;
 
-  const WatchListWidget({super.key, required this.result});
+  const WatchListWidget({Key? key, required this.result});
+
+  @override
+  State<WatchListWidget> createState() => _WatchListWidgetState();
+}
+
+class _WatchListWidgetState extends State<WatchListWidget> {
+  bool isBookmarked = false;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Navigator.pushNamed(context, MovieView.routeName, arguments: result);
+        Navigator.pushNamed(context, MovieView.routeName, arguments: widget.result);
       },
       child: Row(
         children: [
@@ -22,7 +30,7 @@ class WatchListWidget extends StatelessWidget {
             children: [
               CachedNetworkImage(
                 imageUrl:
-                    'https://image.tmdb.org/t/p/w500${result.backdropPath ?? ''}',
+                'https://image.tmdb.org/t/p/w500${widget.result.backdropPath ?? ''}',
                 width: MediaQuery.of(context).size.width * 0.5,
                 height: MediaQuery.of(context).size.height * 0.1,
                 alignment: Alignment.center,
@@ -36,7 +44,7 @@ class WatchListWidget extends StatelessWidget {
                   color: Colors.red,
                 ),
               ),
-              buildIcon()
+              buildIcon(widget.result) // Pass widget.result to buildIcon function
             ],
           ),
           Expanded(
@@ -44,15 +52,15 @@ class WatchListWidget extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  result.title ?? '',
+                  widget.result.title ?? '',
                   style: AppTheme.movieTitle,
                 ),
                 Text(
-                  result.releaseDate ?? '',
+                  widget.result.releaseDate ?? '',
                   style: AppTheme.time,
                 ),
                 Text(
-                  result.originalTitle ?? '',
+                  widget.result.originalTitle ?? '',
                   style: AppTheme.time,
                 )
               ],
@@ -63,14 +71,40 @@ class WatchListWidget extends StatelessWidget {
     );
   }
 
-  Positioned buildIcon() {
+  Positioned buildIcon(Film film) { // Change the argument type to Film
     return Positioned(
         left: 10,
         bottom: 53,
         child: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            toggleBookmark(film);
+          },
           icon: const Icon(Icons.bookmark_add, size: 25),
           color: AppColors.selectIcon,
         ));
+  }
+
+  void toggleBookmark(Film film) {
+    setState(() {
+      isBookmarked = !isBookmarked;
+      if (isBookmarked) {
+        String filmTitle = film.title ?? "";
+        FirebaseUtils.getFilmId(filmTitle).then((filmId) {
+          if (filmId != null) {
+            FirebaseUtils.deleteFilm(filmId).then((value) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Film Removed Successfully.'),
+                ),
+              );
+            }).catchError((error) {
+              print('Error deleting film: $error');
+            });
+          } else {
+            print('Film not found in database.');
+          }
+        });
+      }
+    });
   }
 }
