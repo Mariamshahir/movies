6bf0af8e6fff31de06e780715cebc2afff3736ad
Mariam Shahir <mariamshahir2000@gmail.com>
2024-04-12 07:21:32 +0200

    Finished api and models.

--- lib/models/film.dart
+++ lib/models/film.dart
@@ -1,19 +1,17 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class Film {
   static const String collectionName = "film";
   late String title;
   late String originalTitle;
   late String backdropPath;
   late String releaseDate;
-  late bool isBookmarked;
+  late bool video;
 
   Film({
     required this.backdropPath,
     required this.originalTitle,
     required this.releaseDate,
     required this.title,
-    this.isBookmarked = false,
+    this.video = false,
   });
 
   Film.fromJson(Map<String, dynamic> json) {
@@ -21,7 +19,7 @@ class Film {
     originalTitle = json['original_title'];
     releaseDate = json['release_date'];
     title = json['title'];
-    isBookmarked = json['is_bookmarked'] ?? false;
+    video = json['is_bookmarked'] ?? false;
   }
 
   Map<String, dynamic> toJson() {
@@ -30,24 +28,24 @@ class Film {
     data['original_title'] = originalTitle;
     data['release_date'] = releaseDate;
     data['title'] = title;
-    data['is_bookmarked'] = isBookmarked;
+    data['is_bookmarked'] = video;
     return data;
   }
 
-  static Future<List<Film>> getFilmsFromFirestore() async {
-    try {
-      QuerySnapshot querySnapshot =
-      await FirebaseFirestore.instance.collection(collectionName).get();
-
-      List<Film> films = querySnapshot.docs.map((doc) {
-        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-        return Film.fromJson(data);
-      }).toList();
-
-      return films;
-    } catch (e) {
-      print('Error fetching films from Firestore: $e');
-      throw Exception('Failed to fetch films from Firestore');
-    }
-  }
+  // static Future<List<Film>> getFilmsFromFirestore() async {
+  //   try {
+  //     QuerySnapshot querySnapshot =
+  //     await FirebaseFirestore.instance.collection(collectionName).get();
+  //
+  //     List<Film> films = querySnapshot.docs.map((doc) {
+  //       Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+  //       return Film.fromJson(data);
+  //     }).toList();
+  //
+  //     return films;
+  //   } catch (e) {
+  //     print('Error fetching films from Firestore: $e');
+  //     throw Exception('Failed to fetch films from Firestore');
+  //   }
+  // }
 }
