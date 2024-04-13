23ccd253be5c6c75258ab3ac7691fd6455f52806
Mariam Shahir <mariamshahir2000@gmail.com>
2024-04-13 15:56:10 +0200

    Finished watch list.

--- lib/tabs/watchlist.dart
+++ lib/tabs/watchlist.dart
@@ -1,35 +1,36 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:test_movies/firebase/firebase.dart';
-import 'package:test_movies/models/film.dart';
 import 'package:test_movies/utils/colors_app.dart';
 import 'package:test_movies/utils/theme_app.dart';
 import 'package:test_movies/widgets/app_error.dart';
 import 'package:test_movies/widgets/lodding_app.dart';
-import 'package:test_movies/widgets/watch_list_widget.dart';
+import 'package:test_movies/widgets/movie_details_widget/movie_view_widget.dart';
 
 class WatchListTab extends StatelessWidget {
-  const WatchListTab({Key? key});
+
+  const WatchListTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Film>>(
-      future: FirebaseUtils.getAllMoviesFromFirestore(),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseUtils.getFilmCollection().snapshots(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const LoaddingApp();
         } else if (snapshot.hasError) {
-          return const AppError(error: "Something went wrong. Please try again later.");
-        } else if (snapshot.hasData) {
-          final movies = snapshot.data!;
-          return listWatchList(movies);
+          return const AppError(
+              error: "Something went wrong. Please try again later.");
         } else {
-          return const SizedBox();
+          final films = snapshot.data!.docs;
+          return listWatchList(films);
         }
       },
     );
   }
 
-  Widget listWatchList(List<Film> movies) {
+  Widget listWatchList(List<QueryDocumentSnapshot<Object?>> films) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 17),
       child: Column(
@@ -41,7 +42,7 @@ class WatchListTab extends StatelessWidget {
           ),
           Expanded(
             child: ListView.separated(
-              itemCount: movies.length,
+              itemCount: films.length,
               separatorBuilder: (BuildContext context, int index) {
                 return const Divider(
                   color: AppColors.divider,
@@ -50,12 +51,77 @@ class WatchListTab extends StatelessWidget {
                 );
               },
               itemBuilder: (context, index) {
+                final film = films[index].data() as Map<String, dynamic>;
                 return Padding(
                   padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: WatchListWidget(result: movies[index]),
+                  child: watchListWidget(film, context),
                 );
               },
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget watchListWidget(Map<String, dynamic> film, BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, MovieView.routeName,
+            arguments: film);
+      },
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  CachedNetworkImage(
+                    imageUrl:
+                    "https://image.tmdb.org/t/p/w500${film['backdrop_path']}",
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    height: MediaQuery.of(context).size.height * 0.12,
+                    placeholder: (context, url) => const Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    errorWidget: (_, __, ___) => const Icon(
+                      Icons.image_not_supported_outlined,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Positioned(
+                    left: 5,
+                    bottom: 82,
+                    child: InkWell(
+                      onTap: () {
+                        FirebaseUtils.deleteFilm(film['id'].toString());
+                      },
+                      child: const Icon(
+                        Icons.bookmark_add,
+                        color: AppColors.selectIcon,
+                        size: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 5,),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      film['title'] ?? '',
+                      style: AppTheme.movieTitle,
+                    ),
+                    Text(film['release_date'] ?? '', style: AppTheme.time),
+                    Text(film['original_title'] ?? '', style: AppTheme.time),
+                  ],
+                ),
+              )
+            ],
           ),
         ],
       ),
